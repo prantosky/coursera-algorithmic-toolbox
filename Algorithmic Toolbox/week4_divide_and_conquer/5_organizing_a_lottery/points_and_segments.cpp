f47fd9c8d6aa486a0ebc3a1b5b87036f8b53a02f
pranavchaudhary2310@gmail.com <pranavchaudhary2310@gmail.com>
2020-06-18 13:51:25 +0530

    Add partial Week4 assignment submission files

--- Algorithmic Toolbox/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.cpp
+++ Algorithmic Toolbox/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.cpp
@@ -1,82 +1,76 @@
 #include <algorithm>
 #include <iostream>
-#include <utility>
+#include <unordered_map>
 #include <vector>
-
 using namespace std;
 
-int modified_binary_search(vector<std::pair<int, int>>& ranges, int start,
-						   int end, int point, bool lower) {
-	if (start > end) {
-		if (lower) {
-			return ranges.size();
-		} else {
-			return -1;
-		}
-	}
-
-	int mid = start + (end - start) / 2;
-	if (ranges[mid].first <= point and ranges[mid].second >= point) {
-		if (lower) {
-			return std::min(mid, modified_binary_search(ranges, start, mid - 1,
-														point, lower));
-		} else {
-			return std::max(mid, modified_binary_search(ranges, mid + 1, end,
-														point, lower));
-		}
-	} else if (point < ranges[mid].second) {
-		return modified_binary_search(ranges, start, mid - 1, point, lower);
-	} else if (point > ranges[mid].first) {
-		return modified_binary_search(ranges, mid + 1, end, point, lower);
-	} else {
-		return -1;
-	}
-}
-
-vector<int> fast_count_segments(vector<std::pair<int, int>>& ranges,
-								vector<int>& points) {
+vector<int> naive_count_segments(const vector<pair<int, int>> ranges,
+								 vector<int> points) {
 	vector<int> cnt(points.size());
-	std::sort(ranges.begin(), ranges.end(),
-			  [](const std::pair<int, int>& a, const std::pair<int, int>& b) {
-				  if (a.first == b.first) {
-					  return a.second < b.second;
-				  } else {
-					  return a.first < b.first;
-				  }
-			  });
-
-	// for (auto&& i : ranges) {
-	// 	std::cout << "(" << i.first << "," << i.second << ")"
-	// 			  << " ";
-	// }
-	// std::cout << std::endl;
-
 	for (size_t i = 0; i < points.size(); i++) {
-		auto a = modified_binary_search(ranges, 0, ranges.size() - 1, points[i],
-										true);
-		auto b = modified_binary_search(ranges, 0, ranges.size() - 1, points[i],
-										false);
-		// cout << i << " " << a << " " << b << " " << b - a + 1 << endl;
-		if (a == ranges.size() or b == -1) {
-			cnt[i] = 0;
-		} else {
-			cnt[i] = b - a + 1;
+		for (size_t j = 0; j < ranges.size(); j++) {
+			cnt[i] +=
+				ranges[j].first <= points[i] && points[i] <= ranges[j].second;
 		}
 	}
 	return cnt;
 }
 
+vector<int> fast_count_segments(vector<std::pair<int, int>>& ranges,
+								vector<int>& points) {
+	vector<int> cnt(points.size());
+
+	vector<pair<int, int>> vec;
+	unordered_map<int, int> point_to_counts;
+
+	vec.reserve(ranges.size() * 2 + points.size());
+	point_to_counts.reserve(points.size());
+
+	for (auto&& i : ranges) {
+		vec.push_back(make_pair(i.first, 0));
+		vec.push_back(make_pair(i.second, 2));
+	}
+	for (auto&& i : points) { vec.push_back(make_pair(i, 1)); }
+
+	sort(vec.begin(), vec.end(), [](pair<int, int>& a, pair<int, int>& b) {
+		if (a.first == b.first) return a.second < b.second;
+		else
+			return a.first < b.first;
+	});
+	int count = 0;
+	for (auto&& i : vec) {
+		if (i.second == 0) {
+			count++;
+		} else if (i.second == 2) {
+			count--;
+		} else {
+			point_to_counts.insert(make_pair(i.first, count));
+		}
+	}
+	for (int i = 0; i < points.size(); i++) {
+		cnt[i] = point_to_counts[points[i]];
+	}
+	return cnt;
+}
+
 int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
 	int n, m;
 	std::cin >> n >> m;
-	vector<std::pair<int, int>> ranges(n);
-	for (size_t i = 0; i < ranges.size(); i++) {
-		std::cin >> ranges[i].first >> ranges[i].second;
+	vector<std::pair<int, int>> ranges;
+	ranges.reserve(n);
+	int start, end;
+	for (size_t i = 0; i < n; i++) {
+		std::cin >> start >> end;
+		if (start <= end) ranges.push_back(make_pair(start, end));
 	}
 
 	vector<int> points(m);
 	for (size_t i = 0; i < points.size(); i++) { std::cin >> points[i]; }
-	// use fast_count_segments
-	vector<int> cnt = fast_count_segments(ranges, points);
-	for (size_t i = 0; i < cnt.size(); i++) { std::cout << cnt[i] << ' '; }
+
+	auto cnt1 = fast_count_segments(ranges, points);
+
+	for (size_t i = 0; i < cnt1.size(); i++) { cout << cnt1[i] << " "; }
 }
