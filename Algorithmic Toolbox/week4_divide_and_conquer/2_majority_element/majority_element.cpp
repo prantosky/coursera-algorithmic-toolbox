70606cff833971c953fef057fe5c66eb842c0343
pranavchaudhary2310@gmail.com <pranavchaudhary2310@gmail.com>
2020-06-21 12:29:09 +0530

    Add remaining Week4:Que6 assignment submission file

--- Algorithmic Toolbox/week4_divide_and_conquer/2_majority_element/majority_element.cpp
+++ Algorithmic Toolbox/week4_divide_and_conquer/2_majority_element/majority_element.cpp
@@ -4,17 +4,41 @@
 
 using std::vector;
 
-int get_majority_element(vector<int> &a, int left, int right) {
-	if (left == right) return -1;
-	if (left + 1 == right) return a[left];
-	// write your code here
+long get_majority_element(vector<long> &a, int left, int right) {
+	if (left == right) { return a[left]; }
+
+	if (left + 1 == right) {
+		if (a[left] != a[right]) return -1;
+		else
+			return a[left];
+	}
+
+	int mid = left + (right - left) / 2;
+	int count_x = 0, count_y = 0;
+
+	long x = get_majority_element(a, left, mid);
+	long y = get_majority_element(a, mid + 1, right);
+
+	for (int i = left; i <= right; i++) {
+		if (x == a.at(i)) {
+			count_x++;
+		} else if (y == a.at(i)) {
+			count_y++;
+		}
+	}
+
+	int n = right - left + 1;
+
+	if (count_x > count_y and count_x > n / 2) return x;
+	if (count_x < count_y and count_y > n / 2) return y;
+
 	return -1;
 }
 
 int main() {
 	int n;
 	std::cin >> n;
-	vector<int> a(n);
+	vector<long> a(n);
 	for (size_t i = 0; i < a.size(); ++i) { std::cin >> a[i]; }
-	std::cout << (get_majority_element(a, 0, a.size()) != -1) << '\n';
+	std::cout << (get_majority_element(a, 0, a.size() - 1) != -1) << '\n';
 }
